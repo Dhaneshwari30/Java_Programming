a24bbde09fa97ab048885b4d23985428b1196710
Dhaneshwari Ghewari <shwari2004@gmail.com>
2025-11-20 13:29:35 +0530

    Create program21_1.java

---
+++ Assignments/Assignment_21/program21_1.java
@@ -0,0 +1,26 @@
+//1. Write a program to calculate the product of digits of a number
+
+class Logic
+{
+    void productOfDigits(int num)
+    {
+        int iDigit = 0, iMul = 1;
+
+        while(num != 0)
+        {
+            iDigit = num % 10;
+            iMul = iMul * iDigit;
+            num = num / 10;
+        }
+        System.out.println(iMul);
+    }
+}
+
+class program21_1
+{
+    public static void main(String A[])
+    {
+        Logic obj = new Logic();
+        obj.productOfDigits(234);
+    }
+}
