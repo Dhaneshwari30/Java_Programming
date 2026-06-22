60ac30b6edcb2ffe25556ecb75c9be2fb28ca7ca
Dhaneshwari Ghewari <shwari2004@gmail.com>
2026-06-22 19:16:04 +0530

    Create Assignment6_1.java

---
+++ Assignments/Assignment6/Assignment6_1.java
@@ -0,0 +1,16 @@
+//Accept name and display that full name 
+
+import java.util.*;
+
+class Assignment6_1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the name: ");
+        String str = sobj.nextLine();
+
+        System.out.println(str);
+    }
+}
