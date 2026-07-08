c1aff7c298eb9c648053bece583810ba4507d8f1
Dhaneshwari Ghewari <shwari2004@gmail.com>
2026-07-08 12:38:10 +0530

    Create Assignment17_1.java

---
+++ Assignments/Assignment_17/Assignment17_1.java
@@ -0,0 +1,25 @@
+//Find the sum of digits
+
+import java.util.*;
+
+class Assignment17_1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int iNo = sobj.nextInt();
+        int iDigit = 0;
+        int iSum = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iSum = iSum + iDigit;
+            iNo = iNo / 10;
+        }
+
+        System.out.println("Sum of digits is: "+iSum);
+    }
+}
